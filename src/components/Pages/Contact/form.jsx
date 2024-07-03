ef048358aa2222d6b6937d01edd31492a4c81f9d
SIROJIDDIN <jorisdandii@gmail.com>
2024-07-04 05:00:41 +0900

    BBR feat: emailJs install

--- src/components/Pages/Contact/form.jsx
+++ src/components/Pages/Contact/form.jsx
@@ -1,6 +1,41 @@
+import React, { useRef } from "react";
 import ReactTyped from "react-typed";
+import emailjs from "emailjs-com";
+import Swal from "sweetalert2";
 
 const Forms = () => {
+  const form = useRef(null);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    if (form.current) {
+      emailjs
+        .sendForm(
+          "service_onajonim1971",
+          "template_k32rykm",
+          form.current,
+          "zgby6PiUEsCe9oQQ2"
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+            Swal.fire({
+              position: "top-end",
+              icon: "success",
+              title: "Email sent!! 🎉🎉🎉",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+            form.current?.reset();
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+    }
+  };
+
   return (
     <section>
       <div className="max-h-screen py-6 flex flex-col justify-center sm:py-12 wrap">
@@ -8,6 +43,8 @@ const Forms = () => {
           <div className="absolute inset-0 bg-gradient-to-r from-lightOrange to-lime-600 shadow-lg transform -skew-y-6 hover:-skew-y-0 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl bg-red-300 hover:rotate-0 "></div>
           <div className="relative px-10 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20 transform hover:origin-center">
             <form
+              onSubmit={sendEmail}
+              ref={form}
               className="max-w-md mx-auto"
               action="https://formspree.io/f/xbjvkywl"
               method="post"
@@ -30,7 +67,7 @@ const Forms = () => {
                       type="text"
                       id="floating_outlined"
                       className="block px-2 pb-2 pt-2 w-full text-[16px] text-gray-900 bg-transparent rounded-lg border-2 border-gray-300 dark:text-lime-600 dark:focus:border-orange-500 focus:outline-none focus:ring-0 focus:border-orange-600 peer"
-                      name="full_name"
+                      name="user_name"
                       required
                     />
                     <label
@@ -45,7 +82,7 @@ const Forms = () => {
                       type="email"
                       id="floating_outlined"
                       className="block px-2 pb-2 pt-2 w-full text-[16px] text-gray-900 bg-transparent rounded-lg border-2 border-gray-300 dark:text-orange dark:focus:border-orange-500 focus:outline-none focus:ring-0 focus:border-orange-600 peer"
-                      name="email"
+                      name="user_email"
                       required
                     />
                     <label
@@ -58,7 +95,8 @@ const Forms = () => {
 
                   <div className="relative">
                     <textarea
-                      type="email"
+                      name="message"
+                      type="message"
                       id="floating_outlined"
                       className="block px-2 pb-2 pt-2 w-full text-[16px] text-gray-900 bg-transparent rounded-lg border-2 border-gray-300 dark:text-orange dark:focus:border-orange-500 focus:outline-none focus:ring-0 focus:border-orange-600 peer"
                       placeholder=" "
